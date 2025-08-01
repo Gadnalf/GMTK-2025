485ebcf101f6d8c985aff040a5c41e647ae8a046
Ness Chu <35279510+justness@users.noreply.github.com>
2025-08-01 18:50:35 -0400

    Camera tracks particle (#2)
    * Set camera as child of particle to track
    
    * Speed-based zoom
    
    * Remove debug

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    float zoomMin = 1;
+    float zoomMax = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speedMag = Math.Abs(gameObject.transform.parent.GetComponent<Rigidbody2D>().linearVelocityY);
+        if (speedMag > zoomMin && speedMag < zoomMax)
+        {
+            if (gameObject.GetComponent<Camera>().orthographicSize < Math.Round(speedMag, 2) - 1)
+                gameObject.GetComponent<Camera>().orthographicSize += 0.01f;
+            else if (gameObject.GetComponent<Camera>().orthographicSize > Math.Round(speedMag, 2) + 1)
+                gameObject.GetComponent<Camera>().orthographicSize -= 0.01f;
+        }
+    }
+}
