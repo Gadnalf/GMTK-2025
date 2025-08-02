1f36902383a6b9a507092d73e8d002da264b25f3
GloomyTeaParty <cc.lia.wang@gmail.com>
2025-08-02 14:18:04 -0700

    Adding mass calculation for collision
    Adding a new ObstacleStats object that just contains the mass of the
    obstacle. We then use the particle weight from upgrade to calculate the
    slowdown factor

--- Assets/Scripts/particleCollisionHandler.cs
+++ Assets/Scripts/particleCollisionHandler.cs
@@ -9,16 +9,26 @@ public class particleCollisionHandler : MonoBehaviour
         if (collision.gameObject.CompareTag("BasicObstacle"))
         {
             Debug.Log("Ran into basic obstacle!");
-            TrackManager.instance.particleVelocity /= 2; // Reduce velocity by half
-            Debug.Log("Current Particle Velocity = " + TrackManager.instance.particleVelocity);
-            if (TrackManager.instance.particleVelocity < 0.1f)
-            {
-                //destory the game object if the velocity is too low    
-                Debug.Log("Particle Velocity too low, destroying object.");
-                GameObject.Destroy(gameObject);
-            }
+            HandleObstacleCollision(collision.gameObject);
         }
     }
+
+    void HandleObstacleCollision(GameObject obstacle)
+    {
+        float obstacleMass = obstacle.GetComponent<ObstacleStats>().collisionMass;
+        float particleMass = UpgradeManager.instance.GetValue(UpgradeManager.Upgrades.ParticleWeight);
+        Debug.Log("Pre-collision Velocity = " + TrackManager.instance.particleVelocity);
+        TrackManager.instance.particleVelocity = TrackManager.instance.particleVelocity * particleMass / (obstacleMass + particleMass); // Reduce velocity by half
+        Debug.Log("Particle Mass: " + particleMass + "Obstacle Mass: " + obstacleMass + "Collision Mass Factor = " + particleMass / (obstacleMass + particleMass));
+        Debug.Log("Post-collision Velocity = " + TrackManager.instance.particleVelocity);
+        if (TrackManager.instance.particleVelocity < 0.1f)
+        {
+            //destory the game object if the velocity is too low    
+            Debug.Log("Particle Velocity too low, destroying object.");
+            GameObject.Destroy(gameObject);
+        }
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
     }
