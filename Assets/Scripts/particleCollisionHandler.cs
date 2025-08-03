8d1d1610d78a183363482d0f8b74ec3ef8e26f65
Philip <philip.cai.mao@hotmail.ca>
2025-08-02 20:17:28 -0400

    Update collision handling to use new velocity interface

--- Assets/Scripts/particleCollisionHandler.cs
+++ Assets/Scripts/particleCollisionHandler.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 
-public class particleCollisionHandler : MonoBehaviour
+public class ParticleCollisionHandler : MonoBehaviour
 {
     int comboCount;
+    private ParticleController pc;
+
 
     void Start()
     {
+        pc = GetComponent<ParticleController>();
         comboCount = 0;
     }
     void OnTriggerEnter2D(Collider2D collision)
@@ -26,11 +29,11 @@ public class particleCollisionHandler : MonoBehaviour
     {
         float obstacleMass = obstacle.GetComponent<ObstacleStats>().collisionMass;
         float particleMass = UpgradeManager.instance.GetValue(UpgradeManager.Upgrades.ParticleWeight);
-        Debug.Log("Pre-collision Velocity = " + TrackManager.instance.particleVelocity);
-        TrackManager.instance.particleVelocity = TrackManager.instance.particleVelocity * particleMass / (obstacleMass + particleMass); // Reduce velocity by half
+        Debug.Log("Pre-collision Velocity = " + pc.forwardVelocity);
+        pc.forwardVelocity = pc.forwardVelocity * particleMass / (obstacleMass + particleMass); // Reduce velocity by half
         Debug.Log("Particle Mass: " + particleMass + "Obstacle Mass: " + obstacleMass + "Collision Mass Factor = " + particleMass / (obstacleMass + particleMass));
-        Debug.Log("Post-collision Velocity = " + TrackManager.instance.particleVelocity);
-        if (TrackManager.instance.particleVelocity < 0.1f)
+        Debug.Log("Post-collision Velocity = " + pc.forwardVelocity);
+        if (pc.forwardVelocity < 0.1f)
         {
             //destory the game object if the velocity is too low    
             Debug.Log("Particle Velocity too low, destroying object.");
@@ -44,25 +47,11 @@ public class particleCollisionHandler : MonoBehaviour
         // v0 : just boost velocity
         // We should also boost acceleration, but remove that acceleration when leaving
         float velocityBump = booster.GetComponent<BoosterStats>().boosterVelocityBump;
-        float originalVelocity = TrackManager.instance.particleVelocity;
+        float originalVelocity = pc.forwardVelocity;
         float comboMultiplier = 1.0f + 0.2f * comboCount;
-        TrackManager.instance.particleVelocity += velocityBump * comboMultiplier;
+        pc.forwardVelocity += velocityBump * comboMultiplier;
         Debug.Log("Velocity boosted from " + originalVelocity + "to " + 
-            TrackManager.instance.particleVelocity + " Delta:(Base: " + velocityBump + ", Combo Multiplier: " + comboMultiplier);
+            pc.forwardVelocity + " Delta:(Base: " + velocityBump + ", Combo Multiplier: " + comboMultiplier);
         comboCount += 1;
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-    }
-
-    void OnCollisionStay2D(Collision2D collision)
-    {
-
-    }
-
-    void OnCollisionExit2D(Collision2D collision)
-    {
-
     }
 }
