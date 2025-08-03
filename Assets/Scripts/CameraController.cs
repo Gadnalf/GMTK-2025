1c63d47c21fcf1b6e9c51816c973d8cde54cff99
Philip Cai <32752007+Gadnalf@users.noreply.github.com>
2025-08-03 01:03:14 -0400

    Update camera behavior, add lateral movement to particle as health (#14)

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,21 +1,25 @@
 using System;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
+public class CameraController : MonoBehaviour {
     public float zoomMin = 1;
     public float zoomMax = 10;
+    public ParticleController cameraTarget;
 
     // Update is called once per frame
-    void Update()
-    {
-        float speedMag = Math.Abs(gameObject.transform.parent.GetComponent<Rigidbody2D>().linearVelocityY);
-        if (speedMag > zoomMin && speedMag < zoomMax)
-        {
+    void Update() {
+        float speedMag = Math.Abs(cameraTarget.forwardVelocity);
+        if (speedMag > zoomMin && speedMag < zoomMax) {
             if (gameObject.GetComponent<Camera>().orthographicSize < Math.Round(speedMag, 2) - 1)
                 gameObject.GetComponent<Camera>().orthographicSize += 0.01f;
             else if (gameObject.GetComponent<Camera>().orthographicSize > Math.Round(speedMag, 2) + 1)
                 gameObject.GetComponent<Camera>().orthographicSize -= 0.01f;
         }
+    }
+
+    void LateUpdate() {
+        Vector3 pos = transform.position;
+        pos.y = cameraTarget.transform.position.y;
+        transform.position = pos;
     }
 }
