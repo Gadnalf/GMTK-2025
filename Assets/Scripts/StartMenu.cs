89568bc01f51ed02b37a837f343e1ad6d5325cc6
Ness Chu <vanessa.chu@mail.mcgill.ca>
2025-08-03 01:32:22 -0400

    Quit button

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class ButtonScript : MonoBehaviour
+public class StartMenu : MonoBehaviour
 {
     public string scene;
 
