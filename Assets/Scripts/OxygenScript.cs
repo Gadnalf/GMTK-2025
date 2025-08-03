5cadeac1a4a47c06eb4164a0204f726104bc4e7c
Philip <philip.cai.mao@hotmail.ca>
2025-08-03 02:46:33 -0400

    Add some random angular velocity too for when it's gonna not be a sphere

--- Assets/Scripts/OxygenScript.cs
+++ Assets/Scripts/OxygenScript.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class OxygenScript : MonoBehaviour {
     public float maxOffset = 0.5f;
+    public float maxAngularVelocity = 10f; // Maximum angular velocity in degrees per second
 
     void Start() {
         // Initialize velocity to be offset
         Vector2 initialVelocity = new Vector2(Random.Range(-maxOffset, maxOffset), Random.Range(-maxOffset, maxOffset));
         GetComponent<EphemeralObject>().velocity = initialVelocity;
+        GetComponent<Rigidbody2D>().angularVelocity = Random.Range(-maxAngularVelocity, maxAngularVelocity);
     }
 }
